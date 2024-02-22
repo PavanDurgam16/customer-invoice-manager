e7b218c2a50785bf9251b0923b0427b13c79ca13
PavanDurgam16 <durgampavan2017@gmail.com>
2024-02-22 08:42:57 +0530

    Customer Invoice Manager Application

---
+++ src/main/java/com/dev_neo/customerinvoicemanager/CustomerInvoiceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.dev_neo.customerinvoicemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerInvoiceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerInvoiceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev_neo/customerinvoicemanager/CustomerInvoiceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev_neo.customerinvoicemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerInvoiceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
